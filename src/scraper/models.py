b379d0c0bb6c321664b9099988861369d4182dea
tishmen <topuzov.work@gmail.com>
2016-03-02 20:48:54 +0100

    added initial models and choices

--- src/scraper/models.py
+++ src/scraper/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+from .choices import *
+
+
+class UserAgent(models.Model):
+
+    string = models.TextField(unique=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.string
+
+
+class Proxy(models.Model):
+
+    host = models.GenericIPAddressField(protocol='IPv4')
+    port = models.PositiveIntegerField()
+    online = models.NullBooleanField()
+    google_ban = models.NullBooleanField()
+    last_date_online = models.DateTimeField(null=True, blank=True)
+    last_date_google_ban = models.DateTimeField(null=True, blank=True)
+    region = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    connection_count = models.PositiveIntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'proxies'
+        unique_together = ['host', 'port']
+
+    def __str__(self):
+        return '{}:{}'.format(self.host, self.port)
+
+
+class GoogleSearch(models.Model):
+
+    q = models.CharField(verbose_name='query', max_length=100)
+    cr = models.CharField(
+        verbose_name='country', max_length=9, choices=SEARCH_CR,
+        default='countryUS'
+    )
+    qdr = models.CharField(
+        verbose_name='time period', max_length=3, null=True, blank=True
+    )
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'google searches'
+
+    def __str__(self):
+        return self.q
+
+
+class GooglePage(models.Model):
+
+    search = models.ForeignKey('GoogleSearch')
+    url = models.URLField()
+    html = models.TextField()
+    start = models.PositiveIntegerField(default=0)
+    end = models.PositiveIntegerField(default=0)
+    next_page = models.URLField(null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
+
+
+class GoogleLink(models.Model):
+
+    page = models.ForeignKey('GooglePage')
+    title = models.CharField(max_length=100)
+    snippet = models.TextField()
+    url = models.URLField()
+    rank = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
