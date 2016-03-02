04366db2d24ee29081b5f4bda2158019b4307ca5
tishmen <topuzov.work@gmail.com>
2016-03-02 21:00:05 +0100

    added resource files

--- src/scraper/admin.py
+++ src/scraper/admin.py
@@ -1,3 +1,31 @@
+from import_export import resources
+from import_export.admin import ImportMixin
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserAgent, Proxy, GoogleSearch, GooglePage, GoogleLink
+
+
+class UserAgentResource(resources.ModelResource):
+
+    class Meta:
+        model = UserAgent
+        exclude = ['date_added']
+
+
+class ProxyResource(resources.ModelResource):
+
+    class Meta:
+        model = Proxy
+        exclude = [
+            'online', 'google_ban', 'last_date_online', 'last_date_google_ban',
+            'region', 'country', 'latitude', 'longitude', 'scraper_count',
+            'date_added', 'date_modified'
+        ]
+
+
+class SearchResource(resources.ModelResource):
+
+    class Meta:
+        model = GoogleSearch
+        exclude = ['date_added']
